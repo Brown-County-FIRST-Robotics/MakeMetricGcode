d6ea41c0ea9146f91859073d2cef6ca9c92ebd3c
Mark Finn <mark@mfinn.net>
2023-11-05 10:40:29 -0600

    addded gui

---
+++ main.py
@@ -0,0 +1,61 @@
+import easygui
+
+title = 'Make Metric GCode postprocessor'
+
+extraMessage = ''
+
+try:
+    while 1:
+        extra = '' if not extraMessage else ('\n\n' + extraMessage)
+        choice = easygui.buttonbox(msg=f'{title} - \n\nMake a gcode file into all metric{extra}', title=title,
+                                   choices=('Select File', 'About / Help', 'Exit'), default_choice='Select File', cancel_choice='Exit')
+        if choice == 'About / Help':
+            easygui.textbox(msg='About', title=title, text='''Converts a gcode file to all "G21" / metric / millimeters.
+
+The Richauto A11/B11 controller on the wood shop "axion precision iconic" CNC routers don't work correctly with inch units. 
+
+Without this post-processing script those controllers will read the G20 and use inches for X/Y/Z but NOT for the feed speed.  I'm not sure what it does about IJK.
+
+This script works by replacing any G20 (inch) sections of the gcode with G21 (millimeter) versions of the numbers.  Command codes modified are A, B, C, F, I, J, K, R, U, V, W, X, Y, Z.
+
+If no G20 is in your gcode, this script will not assume inches and it will do nothing.  You could add a G20 at the top of your code and try again.
+
+Comments are currently lost in the output because I'm lazy.
+''')
+            extraMessage = ''
+        elif choice == 'Select File':
+            fname = easygui.fileopenbox(msg='Select file to process', title=title, default='*.nc',
+                                        filetypes=[['*.nc', '*.NC', '*.gc', '*.GC', 'gcode files'], ['*.*', 'All files']], multiple=False)
+            if fname is None:
+                extraMessage = ''
+                continue
+            import make_metric_gcode
+
+            try:
+
+                with open(fname, 'r') as f:
+                    gcode = f.read()
+
+                output = make_metric_gcode.process(gcode)
+
+                newfilename = make_metric_gcode.suggestname(fname)
+
+                newfilename = easygui.filesavebox(msg='Save result as', title=title, default=newfilename,
+                                                  filetypes=[['*.nc', '*.NC', '*.gc', '*.GC', 'gcode files'], ['*.*', 'All files']])
+                if newfilename is None:
+                    extraMessage = 'Last file canceled'
+                    continue
+
+                with open(newfilename, 'w') as f:
+                    f.write('\n'.join(output))
+
+            except:
+                easygui.exceptionbox(title=title, msg='failure processing this gcode file')
+                extraMessage = 'Last file failed'
+
+            extraMessage = 'File processed and saved'
+        else:
+            break
+
+except:
+    easygui.exceptionbox(title=title, msg='general failure')
--- make_metric_gcode.py
+++ make_metric_gcode.py
@@ -1,87 +1,98 @@
-#from gcodeparser import GcodeParser
-import sys
 import re
 
+def process(gcode):
+    '''Converts a gcode file to all "G21" / metric / millimeters.
 
-#the richauto b11/a11 controller on the wood shop "axion precision iconic" CNC routers don't work correctly and needs metric
-#without this post script on the gcode thise controlers it will read the G20 and use inches for x/y/z but NOT for the feed speed.  not sure about IJK.
+The Richauto A11/B11 controller on the wood shop "axion precision iconic" CNC routers don't work correctly with inch units.
 
-fname = sys.argv[1]
+Without this post-processing script those controllers will read the G20 and use inches for X/Y/Z but NOT for the feed speed.  I'm not sure what it does about IJK.
 
-isImperial = True
+This script works by replacing any G20 (inch) sections of the gcode with G21 (millimeter) versions of the numbers.  Command codes modified are A, B, C, F, I, J, K, R, U, V, W, X, Y, Z.
 
+If no G20 is in your gcode, this script will not assume inches and it will do nothing.  You could add a G20 at the top of your code and try again.
 
-outputlines = []
-outputlines.append('G21 (forced to metric by make_metric_gcode.py)')
+Comments are currently lost in the output because I'm lazy.'''
 
-regex = r'([A-Za-z])([-+]?(?:(?:\d+(?:\.\d*)?)|(?:\d*\.\d+)))'
+    isImperial = True
 
-with open(fname, 'r') as f:
-  for line in f:
+    outputlines = []
+    outputlines.append('G21 (forced to metric by make_metric_gcode.py)')
 
-    #deal with comments
-    outline=''
-    parensopen = 0
-    for c in line.rstrip():
-      if c == ';' and not parensopen:
-        break
-      elif c=='(':
-        parensopen += 1
-      elif c==')':
-        parensopen -= 1
-      elif not parensopen:
-        outline += c      
-    line = outline
+    regex = re.compile(r'([A-Za-z])([-+]?(?:(?:\d+(?:\.\d*)?)|(?:\d*\.\d+)))')
 
+    for line in gcode.split('\n'):
 
-    #break up into command/register segments
-    segs = line.split()
-    newline = []
-    for seg in segs:
-#      print(seg)
-      segparts = re.match(regex, seg)
-      if segparts is None:
-        newline.append(seg)
-      else:
-#        print(segparts.groups())
-        letter, number = segparts.groups()
-        isfloat = '.' in number
-        if isfloat:
-          number = float(number)
-        else:
-          number = int(number)
-        if letter.upper() == 'G' and number in [20,21]:
-          if number == 20: #imperial
-            isImperial = True
-          else:
-            isImperial = False
-            continue
+        # deal with comments
+        outline = ''
+        parensopen = 0
+        for c in line.rstrip():
+            if c == ';' and not parensopen:
+                break
+            elif c == '(':
+                parensopen += 1
+            elif c == ')':
+                parensopen -= 1
+            elif not parensopen:
+                outline += c
+        line = outline
 
-#          print(letter, number)
-        if letter.upper() in 'ABCFIJKRUVWXYZ' and isImperial:
-          number *= 25.4
+        # break up into command/register segments
+        segs = line.split()
+        newline = []
+        for seg in segs:
+            #      print(seg)
+            segparts = regex.match(seg)
+            if segparts is None:
+                newline.append(seg)
+            else:
+                #        print(segparts.groups())
+                letter, number = segparts.groups()
+                isfloat = '.' in number
+                if isfloat:
+                    number = float(number)
+                else:
+                    number = int(number)
+                if letter.upper() == 'G' and number in [20, 21]:
+                    if number == 20:  # imperial
+                        isImperial = True
+                    else:
+                        isImperial = False
+                        continue
 
-        if isfloat:
-          newline.append('%s%.3f' % (letter, number))
-        else:
-          newline.append('%s%u' % (letter, number))
+                #          print(letter, number)
+                if letter.upper() in 'ABCFIJKRUVWXYZ' and isImperial:
+                    number *= 25.4
 
-#        print(letter, number)
-    outputlines.append(' '.join(newline))
+                if isfloat:
+                    newline.append('%s%.3f' % (letter, number))
+                else:
+                    newline.append('%s%u' % (letter, number))
 
+        #        print(letter, number)
+        outputlines.append(' '.join(newline))
 
-
-
-if '.' in fname:
-  name,ext = sys.argv[1].rsplit('.', 1)
-  newfilename = name + '_metric.' + ext
-else:
-  newfilename = fname + '_metric.nc'
+    return '\n'.join(outputlines)
 
 
-with open(newfilename, 'w') as f:
-  f.write('\n'.join(outputlines))
- 
-print('wrote', newfilename)
+def suggestname(oldname):
+    if '.' in oldname:
+        name, ext = oldname.rsplit('.', 1)
+        newfilename = name + '_metric.' + ext
+    else:
+        newfilename = oldname + '_metric.nc'
+    return newfilename
 
+if __name__ == '__main__':
+    import sys
 
+    fname = sys.argv[1]
+    with open(fname, 'r') as f:
+        gcode = f.read()
+
+    output = process(gcode)
+
+    newfilename = suggestname(fname)
+    with open(newfilename, 'w') as f:
+        f.write('\n'.join(output))
+
+    print('wrote', newfilename)
