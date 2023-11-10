1a9d5ffe9bafa9334058ba31b47e388aeab2e9cd
Mark Finn <mark@mfinn.net>
2023-11-10 09:11:39 -0600

    fixes and extents

--- make_metric_gcode.py
+++ make_metric_gcode.py
@@ -1,6 +1,6 @@
 import re
 
-def process(gcode):
+def process(gcode, extents=None):
     '''Converts a gcode file to all "G21" / metric / millimeters.
 
 The Richauto A11/B11 controller on the wood shop "axion precision iconic" CNC routers don't work correctly with inch units.
@@ -52,16 +52,27 @@ Comments are currently lost in the output because I'm lazy.'''
                     number = float(number)
                 else:
                     number = int(number)
-                if letter.upper() == 'G' and number in [20, 21]:
+                letter = letter.upper()
+
+                if letter == 'G' and number in [20, 21]:
                     if number == 20:  # imperial
                         isImperial = True
+                        continue
                     else:
                         isImperial = False
                         continue
 
                 #          print(letter, number)
-                if letter.upper() in 'ABCFIJKRUVWXYZ' and isImperial:
-                    number *= 25.4
+                if letter in 'ABCFIJKRUVWXYZ': #distance commands
+                    if isImperial:
+                        number *= 25.4
+
+                    if extents is not None:
+                        if letter in extents:
+                            c = extents[letter]
+                            extents[letter] = min(number, c[0]), max(number, c[1])
+                        else:
+                            extents[letter] = number, number
 
                 if isfloat:
                     newline.append('%s%.3f' % (letter, number))
@@ -71,7 +82,7 @@ Comments are currently lost in the output because I'm lazy.'''
         #        print(letter, number)
         outputlines.append(' '.join(newline))
 
-    return '\n'.join(outputlines)
+    return outputlines
 
 
 def suggestname(oldname):
@@ -89,10 +100,15 @@ if __name__ == '__main__':
     with open(fname, 'r') as f:
         gcode = f.read()
 
-    output = process(gcode)
+    extents={}
+    output = process(gcode, extents)
 
     newfilename = suggestname(fname)
     with open(newfilename, 'w') as f:
         f.write('\n'.join(output))
+
+    print('Extents in mm:')
+    for letter, (mn, mx) in sorted(extents.items()):
+        print(f'{letter}: {mn} to {mx}')
 
     print('wrote', newfilename)
